15cc254543433b482ac62dc2148ffddb7bf12e31
YousraBarhmi <barhmiyousra@gmail.com>
2024-06-01 08:47:48 +0200

    Add setup script to install system dependencies

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@
 
 # Install libGL
 sudo apt-get update && sudo apt-get install -y libgl1-mesa-glx
+
+# Update package lists
+sudo apt-get update
+
+# Install libgl1
+sudo apt-get install -y libgl1-mesa-glx
